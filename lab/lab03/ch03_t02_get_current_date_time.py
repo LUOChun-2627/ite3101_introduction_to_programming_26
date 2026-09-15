bf28d5b2cad773294d46505e22f8c6f087ab95a3
LUOChun-2627 <frankluo0220@gmail.com>
2026-09-15 05:11:20 +0000

    [skip actions] lab/lab03/ch03_t02_get_current_date_time.py

--- lab/lab03/ch03_t02_get_current_date_time.py
+++ lab/lab03/ch03_t02_get_current_date_time.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 now = datetime.now()
-currect_month = now.month
-currect_day = now.day
-currect_year = now.year
 print(currect_year)
 print(currect_month)
 print(currect_day)
