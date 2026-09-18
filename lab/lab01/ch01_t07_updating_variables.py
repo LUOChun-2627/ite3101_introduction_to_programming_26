c11986f5c23dffec4faabfde6e8945f1bd45c73f
LUOChun-2627 <frankluo0220@gmail.com>
2026-09-18 05:32:52 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -8,6 +8,10 @@ august_rainfall = 4.91
 annual_rainfall += august_rainfall
 
 september_rainfall = 5.16
+annual_rainfall +=
 october_rainfall = 7.20
+annual_rainfall +=
 november_rainfall = 5.06
+annual_rainfall +=
 december_rainfall = 4.06
+annual_rainfall +=
