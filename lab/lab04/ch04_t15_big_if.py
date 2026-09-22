c2698f614d33a190a001ff314ad3cf3d2b4f8a57
LUOChun-2627 <frankluo0220@gmail.com>
2026-09-22 02:51:52 +0000

    lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -4,9 +4,9 @@ def grade_converter(grade: int) -> str:
         return "A"
     elif grade >= 80 and grade <= 89:
         return "B"
-    elif grade >= 80 and grade <= 79:
+    elif grade >= 70 and grade <= 79:
         return "C"
-    elif None:
+    elif grade >=65 and grade <= 69:
         return "D"
     else:
         return "F"
